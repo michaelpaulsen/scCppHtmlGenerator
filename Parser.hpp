082eb2a322a2064cb7ac409ed2bf956f3e6b0aff
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-05-07 00:22:20 -0700

    finished ParseLine function
    it parses a single line as a html template.
    Moved ParseLine into a namespace

--- Parser.hpp
+++ Parser.hpp
@@ -5,17 +5,38 @@
   #include <iostream>
   #include <string>
   #include <cstring>
-
-Tag parseLine(char* line){ 
-  std::cout <<"procesing line " << line; 
-  char *token = std::strtok(line, delimiters);
-  int x = 1; 
-  while (token) {
-    std::cout <<'\n'<<token;  
-    token = std::strtok(nullptr, delimiters);
-    x++;    
-  }
-  std::cout<<'\n';
-  return Tag("tempRetVal","TEMPRETVAL");
-} 
+namespace Parser{ 
+  Tag ParseLine(char line[]){ 
+    char* token = std::strtok(line, " ");
+    std::string content;
+    std::string element;
+    std::string classes;
+    std::string id;
+    int x = 1; 
+    
+    while (token) {
+    
+      if(x == 1){ 
+          element = token; 
+      }else if( token[0] == '.' || token[0] == '#'){
+        char* m = (token+sizeof(char));
+        if(token[0] == '.'){
+            if(classes != "\0"){ 
+              classes+= " "; 
+            }
+            classes+= m;
+          }else{ 
+            id = m; 
+          }
+      }
+     token = std::strtok(nullptr, " ");
+      x++;    
+    }
+    #ifdef DEBUG 
+    printf("el : %s, id : %s, classes: %s, content: %s\n",element.c_str(), id.c_str(), classes.c_str(), content.c_str());
+    #endif
+    return Tag(element,id).AddClass(classes).SetContent(content);
+  
+  } 
+}
 #endif
