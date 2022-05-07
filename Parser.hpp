45d763ebbc9c95bb669bd8842003c8329526c6be
michaelpaulsen <michael.j.paulsen@gmail.com>
2022-05-07 15:44:32 -0700

    7/5/22 save

--- Parser.hpp
+++ Parser.hpp
@@ -32,11 +32,40 @@ namespace Parser{
      token = std::strtok(nullptr, " ");
       x++;    
     }
-    #ifdef DEBUG 
-    printf("el : %s, id : %s, classes: %s, content: %s\n",element.c_str(), id.c_str(), classes.c_str(), content.c_str());
-    #endif
     return Tag(element,id).AddClass(classes).SetContent(content);
   
   } 
+   void ParseFile(char* fn, Tag* outTag){ 
+    Tag* currentElement = nullptr;
+    int layer = 0; 
+    std::ifstream fp; 
+    fp.open(fn);
+    char line[1000]; 
+    while(fp.getline(line, 1000, '\n')){ 
+      auto t = ParseLine(line);
+      if(line[0] == '/' ){// if the line starts with a dash then close the tag. 
+        if(currentElement->GetParentTag() != nullptr){ 
+        // not having this if statment caused a huge bug in the code  
+          currentElement = currentElement->GetParentTag();
+        }else{ 
+        printf("reached top level element %s\n",(currentElement->GetTagName()).c_str());  
+        }
+        layer--; 
+        continue;  
+      }
+      if(layer++ == 0 ){ //if the element is the top level element
+        //incrments layer every time the loop gets this far;  
+        currentElement = &t;
+        continue; 
+      } 
+      currentElement->AddTag_v(t); 
+      currentElement = &t; 
+    }
+    fp.close();
+     //memcpy(outTag,currentElement,sizeof(Tag)); 
+    outTag = currentElement;
+     if(outTag == nullptr ) printf("something whent wrong");
+  }
 }
+
 #endif
